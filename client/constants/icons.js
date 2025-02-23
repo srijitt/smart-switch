ade374d80eb25f99f07b3180a55103961200e1a1
srijitt <srijit.chakraborty154@gmail.com>
2025-02-23 11:03:56 +0530

    frontend macros

---
+++ client/constants/icons.js
@@ -0,0 +1,37 @@
+import home from "../assets/icons/home.png";
+import plus from "../assets/icons/plus.png";
+import profile from "../assets/icons/profile.png";
+import leftArrow from "../assets/icons/left-arrow.png";
+import menu from "../assets/icons/menu.png";
+import search from "../assets/icons/search.png";
+import rightArrow from "../assets/icons/right-arrow.png";
+import logout from "../assets/icons/logout.png";
+import eyeHide from "../assets/icons/eye-hide.png";
+import eye from "../assets/icons/eye.png";
+import poweron from "../assets/icons/poweron.png";
+import poweroff from "../assets/icons/poweroff.png";
+import on from "../assets/icons/on.png";
+import off from "../assets/icons/off.png";
+import edit from '../assets/icons/edit.png';
+import support from '../assets/icons/support.png';
+import deleteIcon from '../assets/icons/delete.png';
+
+export default {
+  home,
+  plus,
+  profile,
+  leftArrow,
+  menu,
+  search,
+  rightArrow,
+  logout,
+  eyeHide,
+  eye,
+  poweron,
+  poweroff,
+  on,
+  off,
+  edit,
+  deleteIcon,
+  support
+};
---
+++ client/constants/images.js
@@ -0,0 +1,5 @@
+import cards from "../assets/images/cards.png";
+import logo from "../assets/images/logo.png";
+import empty from "../assets/images/empty.png";
+
+export default {cards, logo, empty };
