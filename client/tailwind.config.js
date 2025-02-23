298621151ce9f76ec28f150ccd7bdf8d12de5377
srijitt <srijit.chakraborty154@gmail.com>
2025-02-23 11:03:09 +0530

    frontend macros

---
+++ client/tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#161622",
+          100: "#27273C"
+        },
+        secondary: {
+          DEFAULT: "#AD49E1",
+          100: "#C26BF0",
+          200: "#8C22C3",
+        },
+        black: {
+          DEFAULT: "#000",
+          100: "#1E1E2D",
+          200: "#232533",
+        },
+        accent: "#EBD3F8",
+        alert: "#FB4C4C",
+        white: {
+          DEFAULT: "#EBD3F8",
+          100: "#E5E5DB",
+        },
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
